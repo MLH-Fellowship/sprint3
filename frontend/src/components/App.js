ffe9e8659143d79a43f722edfe476c69763b860e
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-04 09:33:53 -0500

    Clean Up Code for Master Branch

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,6 @@
 import React, { useEffect } from 'react';
-import backend from '../api/backend';
 
 const App = () => {
-
-    // function to search for term and update search results
-    const search = async (term) => {
-        const response = await backend.get(`/search/${term}`);
-        console.log(response.data.tracks.items)
-    }
-    useEffect(() => {
-        search('zindagi')
-    }, [])
 
     return (
         <div>
