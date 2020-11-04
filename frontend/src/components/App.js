25e2fe8e9933b38d3a0ed399d1716dd2c4f3a23c
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-04 09:42:19 -0500

    Add Search Directory and SearchBar Component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 
+import SearchBar from './Search/SearchBar';
+
 const App = () => {
 
     return (
         <div>
             APP
+            <SearchBar />
         </div>
     );
 };
