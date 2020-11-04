5ab18aafc6f718e7ad05cf08f9943500997e5747
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-04 09:32:40 -0500

    Add Backend API and Connect with Frontend

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import backend from '../api/backend';
 
 const App = () => {
+
+    // function to search for term and update search results
+    const search = async (term) => {
+        const response = await backend.get(`/search/${term}`);
+        console.log(response.data.tracks.items)
+    }
+    useEffect(() => {
+        search('zindagi')
+    }, [])
+
     return (
         <div>
             APP
