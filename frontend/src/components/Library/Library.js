299b15751071bc885aca8aa9399795f240289c32
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-04 23:36:09 -0500

    Create Library Component (as Table)

--- frontend/src/components/Library/Library.js
+++ frontend/src/components/Library/Library.js
@@ -4,23 +4,53 @@ const Library = ({ library, removeTrack }) => {
 
     // loop through tracks to create elements for rendering
     const tracksRendered = library.map(track => {
-        const { id, name } = track;
+        const { id, name, artists, duration_ms } = track;
+
+         // extract artist names from artists array
+         const artistNames = artists.map(artist => artist.name);
 
         return (
-            <div key={id}>
-                <div className="title">
-                {name}
-                {/*<button className="ui negative icon button" onClick={() => removeTrack(track)}>
-                    <i className="minus icon" />
-                </button>*/}
-                </div>
-            </div>
+            //<div key={id}>
+            //    <div className="title">
+            //        <i className="dropdown icon" />
+            //    {/*<button className="ui negative icon button" onClick={() => removeTrack(track)}>
+            //        <i className="minus icon" />
+            //    </button>*/}
+            //    </div>
+            //    <div className="content">
+            //        {name}
+            //    </div>
+            //</div>
+            <>
+                <tr>
+                    <td>{name}</td>
+                    <td>{artistNames}</td>
+                    <td>{Math.round(duration_ms/60000)} minutes</td>
+                    <td>
+                        <button 
+                            className="ui mini negative icon button"
+                            onClick={() => removeTrack(track)}
+                        >
+                            <i className="x icon" />
+                        </button>
+                    </td>
+                </tr>
+            </>
         )
     })
 
     return (
-        <div className="ui styled accordion">
-            {tracksRendered}
+        <div className="ui celled table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Artist(s)</th>
+                    <th>Duration</th>
+                </tr>
+            </thead>
+            <tbody>
+                {tracksRendered}
+            </tbody>
         </div>
     );
 };
