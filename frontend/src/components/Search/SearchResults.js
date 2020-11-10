5721df2d41bc778d69123ca2eeed0f247273c8a4
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-10 16:35:45 -0500

    Initialize Checkboxes in SearchResults and Add Disable Logic

--- frontend/src/components/Search/SearchResults.js
+++ frontend/src/components/Search/SearchResults.js
@@ -1,34 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/searchresult.css';
 
 const SearchResults = ({ searchResults, addTrack, removeTrack, library }) => {
 
-    // component styles
-    const searchResultStyles = {
-        songCard: {
-            backgroundColor: '#191414',
-        },
-        songContent: {
-            color: '#ffffff',
-            margin: '10px 0px 10px 5px',
-        },
-        songMeta: {
-            color: '#beb8b8',
-            margin: '0px 0px 5px 5px',
-        },
-        songButton: {
-            backgroundColor: '#1db954',
-            color: '#ffffff',
-        },
-    };
+    // state for songs to be added to/deleted from library
+    const [tracksToTrack, setTracksToTrack] = useState([]);
 
-    // loop through search results (arrary)
-    const resultsRendered = searchResults.map(result => {
-        const { id, name, artists, duration_ms, album } = result;
-        const sep = '| ';
+    // state for 'select all' checkbox
+    const [isCheckedSelectAll, setIsCheckedSelectAll] = useState(false);
+    // text to display for select/deselect
+    const selectAllText = isCheckedSelectAll ? 'Deselect All' : 'Select All';
 
-        // extract artist names from artists array
-        const artistNames = artists.map(artist => artist.name);
+    // function to disable checkbox if track already in library
+    const disableCheckbox = track => {
+        const { id } = track;
+        return library.map(t => t.id).includes(id);
+    }
+
+    
+    // loop through tracks to create elements for rendering
+    const tracksRendered = searchResults.map(track => {
+        const { id, name, artists, duration_ms, audioFeatures } = track;
+
+         // extract artist names from artists array
+         const artistNames = artists.map(artist => artist.name);
+         const artistString = artistNames.join(', ');
+
+         // calculated song duration in minutes and seconds
+         const duration_minutes = Math.floor(duration_ms/60000);
+         const duration_seconds = duration_ms % 60;
+         const duration_seconds_pad = duration_seconds > 10 ? '' : '0'
+         const duration_label = `${duration_minutes}:${duration_seconds_pad}${duration_seconds}`;
 
         // boolean for whether or not song is in library
         const isInLibrary = library.map(track => track.id).includes(id);
@@ -37,37 +39,128 @@ const SearchResults = ({ searchResults, addTrack, removeTrack, library }) => {
         buttonObj['sign'] = isInLibrary ? 'negative' : 'positive';
         buttonObj['func'] = isInLibrary ? removeTrack : addTrack;
         buttonObj['icon'] = isInLibrary ? 'minus' : 'add';
-        buttonObj['label'] = isInLibrary ? 'Remove' : 'Add';
 
         return (
-            <div key={id} className='card song-card' style={searchResultStyles.songCard}>
-                <div className='content'>
-                    <div className='header' style={searchResultStyles.songContent}>
-                        <h2>{name}</h2>
-                    </div>
-                    <div className='meta' style={searchResultStyles.songMeta}>
-                        <span>{Math.round(duration_ms / 60000)} minutes</span>
-                        {sep}
-                        <span>{artistNames.join(', ')}</span>
-                    </div>
-                </div>
+            <React.Fragment key={id}>
+                <tr>
+                    <td>
+                        <input 
+                            type="checkbox"
+                            checked={false}
+                            disabled={disableCheckbox(track)}
+                        />
+                    </td>
+                    <td>
+                        <button 
+                            className={`ui ${buttonObj['sign']} icon button`} 
+                            onClick={() => buttonObj['func'](track)}
+                        >
+                            <i className={`${buttonObj['icon']} icon`} />
+                        </button>
+                    </td>
+                    <td>{name}</td>
+                    <td>{artistString}</td>
+                    <td>{duration_label}</td>
+                    {/*<td>{JSON.stringify(audioFeatures)}</td>*/}
+                   
+                </tr>
+            </React.Fragment>
+        )
+    })
 
-                <div className='image'>
-                    <img src={album.images[1].url} alt={name} />
-                </div>
-                <button className={`ui ${buttonObj['sign']} button`} onClick={() => buttonObj['func'](result)}>
-                    <i className={`${buttonObj['icon']} icon`} />
-                    {`${buttonObj['label']} Track`}
-                </button>
-            </div>
-        );
-    });
 
     return (
         <div>
-            <div className='ui four cards'>{resultsRendered}</div>
+            <button>Add to Library</button>
+            <button>Remove from Library</button>
+            <table className="ui celled table">
+                <thead>
+                    <tr>
+                        <th>
+                            <input 
+                                type="checkbox"
+                                onChange={() => setIsCheckedSelectAll(!isCheckedSelectAll)}
+                            /> {selectAllText}
+                        </th>
+                        <th>Add/Remove</th>
+                        <th>Name</th>
+                        <th>Artist(s)</th>
+                        <th>Duration</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tracksRendered}
+                </tbody>
+            </table>
         </div>
+
     );
+
+    //// component styles
+    //const searchResultStyles = {
+    //    songCard: {
+    //        backgroundColor: '#191414',
+    //    },
+    //    songContent: {
+    //        color: '#ffffff',
+    //        margin: '10px 0px 10px 5px',
+    //    },
+    //    songMeta: {
+    //        color: '#beb8b8',
+    //        margin: '0px 0px 5px 5px',
+    //    },
+    //    songButton: {
+    //        backgroundColor: '#1db954',
+    //        color: '#ffffff',
+    //    },
+    //};
+
+    //// loop through search results (arrary)
+    //const resultsRendered = searchResults.map(result => {
+    //    const { id, name, artists, duration_ms, album } = result;
+    //    const sep = '| ';
+
+    //    // extract artist names from artists array
+    //    const artistNames = artists.map(artist => artist.name);
+
+    //    // boolean for whether or not song is in library
+    //    const isInLibrary = library.map(track => track.id).includes(id);
+    //    // logic for button properties
+    //    const buttonObj = {};
+    //    buttonObj['sign'] = isInLibrary ? 'negative' : 'positive';
+    //    buttonObj['func'] = isInLibrary ? removeTrack : addTrack;
+    //    buttonObj['icon'] = isInLibrary ? 'minus' : 'add';
+    //    buttonObj['label'] = isInLibrary ? 'Remove' : 'Add';
+
+    //    return (
+    //        <div key={id} className='card song-card' style={searchResultStyles.songCard}>
+    //            <div className='content'>
+    //                <div className='header' style={searchResultStyles.songContent}>
+    //                    <h2>{name}</h2>
+    //                </div>
+    //                <div className='meta' style={searchResultStyles.songMeta}>
+    //                    <span>{Math.round(duration_ms / 60000)} minutes</span>
+    //                    {sep}
+    //                    <span>{artistNames.join(', ')}</span>
+    //                </div>
+    //            </div>
+
+    //            <div className='image'>
+    //                <img src={album.images[1].url} alt={name} />
+    //            </div>
+    //            <button className={`ui ${buttonObj['sign']} button`} onClick={() => buttonObj['func'](result)}>
+    //                <i className={`${buttonObj['icon']} icon`} />
+    //                {`${buttonObj['label']} Track`}
+    //            </button>
+    //        </div>
+    //    );
+    //});
+
+    //return (
+    //    <div>
+    //        <div className='ui four cards'>{resultsRendered}</div>
+    //    </div>
+    //);
 };
 
 export default SearchResults;
